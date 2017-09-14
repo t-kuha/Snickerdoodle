e74e6a5804751f99a9d8aeea5696e1d4d0913033
t-kuha <imagingtechnerd@gmail.com>
2017-09-14 11:00:09 +0000

    Under construction

--- simple/build_sw.sh
+++ simple/build_sw.sh
@@ -2,8 +2,12 @@
 
 echo "... Building petalinux for Snickerdoodle Black ..."
 
+# <root>/petalinux/sd_blk/<...>
+
+
+PF_NAME=sd_blk
+PROJ_ROOT=petalinux
 PETALINUX_ROOT=/home/imagingtechnerd/xilinx/2017.1
-PROJECT_NAME=sd_blk
 
 # Source petalinux & Yocto tools
 source ${PETALINUX_ROOT}/settings.sh
@@ -12,37 +16,55 @@ source ${PETALINUX_ROOT}/components/yocto/source/arm/layers/core/oe-init-build-e
 export PATH=${PETALINUX_ROOT}/tools/hsm/bin:${PATH}
 export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE PETALINUX"
 
+
+# Preparation
+DIR_OUTPUT=output
+DIR_TMP=tmp
+DIR_DOWNLOAD=download
+DIR_PF_HW=platform/hw
+DIR_PF_SW=platform/sw
+DIR_VIVADO=vivado
+
+#mkdir -p ${DIR_OUTPUT}
+mkdir -p ${DIR_TMP}
+#mkdir -p ${DIR_DOWNLOAD}
+mkdir -p ${DIR_PF_HW} ${DIR_PF_HW}/vivado
+mkdir -p ${DIR_PF_SW}
+
+
 # Download linux kernel for SDSoC
 KERNEL_SRC_NAME=xilinx-v2016.4-sdsoc.tar.gz
 
 if 
 
 echo "Downloading Linux Kernel source for SDSoC..."
-if [ ! -e download ]; then
-	mkdir -p download 
+if [ ! -e ${DIR_DOWNLOAD} ]; then
+	mkdir -p ${DIR_DOWNLOAD} 
 fi
 
-if [ ! -f ./download/${KERNEL_SRC_NAME} ]; then
-	wget https://github.com/Xilinx/linux-xlnx/archive/xilinx-v2016.4-sdsoc.tar.gz -O ./download/${KERNEL_SRC_NAME}
+if [ ! -f ./${DIR_DOWNLOAD}/${KERNEL_SRC_NAME} ]; then
+	wget https://github.com/Xilinx/linux-xlnx/archive/xilinx-v2016.4-sdsoc.tar.gz -O ./${DIR_DOWNLOAD}/${KERNEL_SRC_NAME}
 fi
 
 
-tar xf ./download/${KERNEL_SRC_NAME} #-C kernel
+tar xf ./${DIR_DOWNLOAD}/${KERNEL_SRC_NAME} -C ${DIR_TMP}
 # Untar-ed folder name: linux-xlnx-xilinx-v2016.4-sdsoc
 
 
 # Building
-#rm -rf ${PROJECT_NAME}
-petalinux-create --type project --template zynq --name ${PROJECT_NAME}
-cd ./${PROJECT_NAME}
-petalinux-config --get-hw-description=..
+#rm -rf ${PF_NAME}
+cd ${PROJ_ROOT}
+
+petalinux-create --type project --template zynq --name ${PF_NAME}
+
+petalinux-config --get-hw-description=${DIR_PF_HW}/vivado/${PF_NAME}.sdk/${PF_NAME}_wrapper.hdf
+
 # https://www.xilinx.com/support/answers/69126.html
 # ps7_uart_1 -> ps7_uart_0
 # vi ./build/tmp/work/plnx_arm-xilinx-linux-gnueabi/device-tree-generation/xilinx+gitAUTOINC+94fc615234-r0/device-tree-generation.yaml
 petalinux-build
 
 # Create petalinux BSP if needed
-cd ..
-petalinux-package --bsp -p ${PROJECT_NAME} --output ${PROJECT_NAME}.bsp
+petalinux-package --bsp -p ${PF_NAME} --output ${DIR_OUTPUT}/${PF_NAME}.bsp
 
 echo "... Done ..."
