7ba267d3f9f1ab4957bc64715486b2d45102ed59
t-kuha <imagingtechnerd@gmail.com>
2018-03-20 18:37:24 +0900

    Update sdx_pfm.tcl

--- src/platform/sdx_pfm.tcl
+++ src/platform/sdx_pfm.tcl
@@ -1,8 +1,20 @@
 # SDSoC platform generation
 
+# Check # of input arguments
+
+
+
 # Setting
 ## Platform name
-set PF_NAME       sdb_simple
+set PF_NAME         [lindex $argv 0]
+# set PF_NAME       sdb_simple
+
+## Image data
+set DIR_IMAGE       [lindex $argv 1]
+
+## Prebuilt data
+set DIR_PREBUILT    [lindex $argv 2]
+
 ## Source
 set DIR_SRC       src
 ## Vivado project directory
@@ -16,12 +28,13 @@ set DIR_PFM       platform
 platform -name ${PF_NAME} \
 -hw ${DIR_VIVADO}/${PF_NAME}.dsa \
 -out ${DIR_PFM} \
--prebuilt;platform -write
+-prebuilt ${DIR_PREBUILT}
+# ;platform -write
 
 # platform -read {/home/pentaxmedical/sdx/workspace/sdb_simple/platform.spr}
 system \
--name {Linux} \
--display-name {Linux} \
+-name {linux} \
+-display-name {linux} \
 -desc {} \
 -boot ${DIR_PETALINUX}/images/linux;boot \
 -bif  ${DIR_SRC}/platform/linux.bif
@@ -32,13 +45,14 @@ system \
 ::scw::get_supported_proc -os standalone
 
 domain \
--name {Linux} \
+-name {linux} \
 -os {linux} \
 -proc {ps7_cortexa9} \
--display-name {Linux} \
+-display-name {linux} \
 -desc {} \
 -runtime {cpp} \
--image ${DIR_PETALINUX}/images/linux
+-image ${DIR_IMAGE}
+#/images/linux
 
 platform -write
 platform -generate
